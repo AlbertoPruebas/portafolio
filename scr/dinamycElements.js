4fac4289136e082b80eb091f4c4999902ba53795
Your Name <albertosoto.lab@gmail.com>
2021-08-03 19:11:08 -0500

    timeline_ok

--- scr/dinamycElements.js
+++ scr/dinamycElements.js
@@ -1,211 +1,155 @@
 document.addEventListener("keydown", e => {
-	if(document.querySelector("#modalBckgrnd")){
-		if (e.key == "Escape") {
-			hiddeModal();
-			return;
-		}
-		if (e.key != "Tab") return;
+    if (document.querySelector("#modalBckgrnd")) {
+        if (e.key == "Escape") {
+            hiddeModal();
+            return;
+        }
+        if (e.key != "Tab") return;
         let first = document.querySelector("#modalBckgrnd").firstChild.firstElementChild;
         let last = document.querySelector("#modalBckgrnd").firstChild.lastElementChild;
-		if (first == e.target && e.shiftKey == true) {
-			e.preventDefault();
-			last.focus();
-		}
-		if (last == e.target && e.shiftKey == false) {
-			e.preventDefault();
-			first.focus();
-		}
-	}
+        if (first == e.target && e.shiftKey == true) {
+            e.preventDefault();
+            last.focus();
+        }
+        if (last == e.target && e.shiftKey == false) {
+            e.preventDefault();
+            first.focus();
+        }
+    }
 });
 
 const showContact = () => {
-    let inputName = createInput("text","contactData","name","Nombre");
-    let inputTel = createInput("text","contactData","noTel","Telefono");
-    let inputEmail = createInput("email","contactData","email","Correo Electronico");
-    let textComments = createTextarea("contactData","comments","Comentarios",5,22);
-    let btnSend = createButton("btnCmnts","btnCmnts","Enviar");
-	let btnClose = createButton("btnCmnts","btnClose","&times");
+    let inputName = createInput("text", "contactData", "name", "Nombre");
+    let inputTel = createInput("text", "contactData", "noTel", "Telefono");
+    let inputEmail = createInput("email", "contactData", "email", "Correo Electronico");
+    let textComments = createTextarea("contactData", "comments", "Comentarios", 5, 22);
+    let btnSend = createButton("btnCmnts", "btnCmnts", "Enviar");
+    let btnClose = createButton("btnCmnts", "btnClose", "&times");
     btnClose.addEventListener("click", () => hiddeModal());
-    showModal([btnClose,inputName,inputTel,inputEmail,textComments,btnSend])
+    showModal([btnClose, inputName, inputTel, inputEmail, textComments, btnSend])
 }
 
-const showModal = (elments)=>{
-	let cont = document.createElement("div");
-	cont.className = "modal";
-	cont.tabIndex="-1"
-	cont.id = "modalBckgrnd";
-	let modal = document.createElement("div");
-	modal.id = "modalElements";
-	elments.forEach(e => {
-		modal.appendChild(e);
-	})
-	cont.appendChild(modal);
-	document.body.appendChild(cont);
-	setTimeout(() => document.querySelector("#modalBckgrnd").style.opacity="1",1);
-	document.querySelector("#modalBckgrnd").focus();
+const showModal = (elments) => {
+    let cont = document.createElement("div");
+    cont.className = "modal";
+    cont.tabIndex = "-1"
+    cont.id = "modalBckgrnd";
+    let modal = document.createElement("div");
+    modal.id = "modalElements";
+    elments.forEach(e => {
+        modal.appendChild(e);
+    })
+    cont.appendChild(modal);
+    document.body.appendChild(cont);
+    setTimeout(() => document.querySelector("#modalBckgrnd").style.opacity = "1", 1);
+    document.querySelector("#modalBckgrnd").focus();
 }
 
 const hiddeModal = () => {
-	let modal = document.querySelectorAll("#modalBckgrnd");
-	if(modal.length > 0){
-        modal[0].style.opacity="0"
-        setTimeout(() => document.body.removeChild(modal[0]),401);
+    let modal = document.querySelectorAll("#modalBckgrnd");
+    if (modal.length > 0) {
+        modal[0].style.opacity = "0"
+        setTimeout(() => document.body.removeChild(modal[0]), 401);
     }
 }
 
 const createInput = (_type, _class, _id, _placeholder) => {
     let input = document.createElement("input");
-    if(_type != undefined) input.type = _type;
-    if(_class != undefined) input.className = _class;
-    if(_id != undefined) input.id = _id;
-    if(_placeholder != undefined) input.placeholder = _placeholder;
+    if (_type != undefined) input.type = _type;
+    if (_class != undefined) input.className = _class;
+    if (_id != undefined) input.id = _id;
+    if (_placeholder != undefined) input.placeholder = _placeholder;
     return input;
 }
 
 const createTextarea = (_class, _id, _placeholder, _rows, _cols) => {
     let text = document.createElement("textarea");
-    if(_class != undefined) text.className = _class;
-    if(_id != undefined) text.id = _id;
-    if(_placeholder != undefined) text.placeholder = _placeholder;
-    if(_rows != undefined) text.rows = _rows;
-    if(_cols != undefined) text.cols = _cols;
+    if (_class != undefined) text.className = _class;
+    if (_id != undefined) text.id = _id;
+    if (_placeholder != undefined) text.placeholder = _placeholder;
+    if (_rows != undefined) text.rows = _rows;
+    if (_cols != undefined) text.cols = _cols;
     return text;
 }
 
 const createButton = (_class, _id, _text) => {
     let btn = document.createElement("button");
-    if(_class != undefined) btn.className = _class;
-    if(_id != undefined) btn.id = _id;
-    if(_text != undefined) btn.innerHTML = _text
+    if (_class != undefined) btn.className = _class;
+    if (_id != undefined) btn.id = _id;
+    if (_text != undefined) btn.innerHTML = _text
     return btn;
 }
 
-const writeLine = async (text,color,nowait = false,permanent = true) => {
+
+
+
+const writeLine = async (text, color, nowait = false, permanent = true) => {
     let p = document.querySelector(".lineTerminal");
     p.style.color = color;
-    for(let c=0;c<text.length;c++){
+    for (let c = 0; c < text.length; c++) {
         p.innerHTML = p.innerHTML + text[c];
-        if(!nowait) await wait(Math.random()*500);
+        if (!nowait) await wait(Math.random() * 300);
     }
-    if(permanent){
+    if (permanent) {
         p.classList.toggle("lineTerminal");
         newTerminalLine();
     }
 }
 
-const newTerminalLine = () =>{
+const newTerminalLine = () => {
     document.querySelector("#cursor").remove();
     let newLine = document.createElement("label");
-    newLine.classList.add("line","lineTerminal");
+    newLine.classList.add("line", "lineTerminal");
     document.querySelector(".console").appendChild(newLine);
     let newCursor = document.createElement("label");
-    newCursor.id ="cursor";
+    newCursor.id = "cursor";
     newCursor.innerText = "|";
     document.querySelector(".console").appendChild(newCursor);
 }
 
-const principalView = (e) => {
-    let selectors = {
-            id: e,
-            item: e.find(".timeline-item"),
-            activeClass: "timeline-item--active",
-            img: ".timeline__img"
-        };
-    selectors.item.eq(0).addClass(selectors.activeClass);
-    selectors.id.css(
-        "background-image",
-        "url(" +
-            selectors.item
-            .first()
-            .find(selectors.img)
-            .attr("src") +
-            ")"
-    );
-}
+const elementTimeline = (data) => {
+    let divTime = document.createElement("div");
+    divTime.className = "swiper-slide"
+    divTime.style.backgroundImage = data.img
 
-const scrollView = (i) => {
-    min = $(this).offset().top;
-    max = $(this).height() + $(this).offset().top;
-    var that = $(this);
-    if (i == itemLength - 2 && pos > min + $(this).height() / 2) {
-      selectors.item.removeClass(selectors.activeClass);
-      selectors.id.css(
-        "background-image",
-        "url(" +
-          selectors.item
-            .last()
-            .find(selectors.img)
-            .attr("src") +
-          ")"
-      );
-      selectors.item.last().addClass(selectors.activeClass);
-    } else if (pos <= max - 40 && pos >= min) {
-      selectors.id.css(
-        "background-image",
-        "url(" +
-          $(this)
-            .find(selectors.img)
-            .attr("src") +
-          ")"
-      );
-      selectors.item.removeClass(selectors.activeClass);
-      $(this).addClass(selectors.activeClass);
-    }
+    let divContent = document.createElement("div");
+    divContent.className = "swiper-slide-content"
+
+    let spanYear = document.createElement("span");
+    spanYear.className = "timeline-year";
+    spanYear.innerText = data.year;
+
+    let h4Title = document.createElement("h4");
+    h4Title.className = "timeline-title";
+    h4Title.innerText = data.title;
+
+    let pText = document.createElement("p");
+    pText.className = "timeline-text";
+    pText.innerText = data.text;
+
+    divContent.appendChild(spanYear);
+    divContent.appendChild(h4Title);
+    divContent.appendChild(pText);
+    divTime.appendChild(divContent);
+
+    document.querySelector(".swiper-wrapper").appendChild(divTime)
 }
 
 
-(function($) {
-    $.fn.timeline = function() {
-      var selectors = {
-        id: $(this),
-        item: $(this).find(".timeline-item"),
-        activeClass: "timeline-item--active",
-        img: ".timeline__img"
-      };
-      selectors.item.eq(0).addClass(selectors.activeClass);
-      selectors.id.css(
-        "background-image",
-        "url(" +
-          selectors.item
-            .first()
-            .find(selectors.img)
-            .attr("src") +
-          ")"
-      );
-      var itemLength = selectors.item.length;
-      $(".timelineScroll").scroll(function() {
-        var max, min;
-        var pos = $(".timelineScroll").scrollTop();
-        selectors.item.each(function(i) {
-          min = $(this).offset().top;
-          max = $(this).height() + $(this).offset().top;
-          if (i == itemLength - 2 && pos > min + $(this).height() / 2) {
-            selectors.item.removeClass(selectors.activeClass);
-            selectors.id.css(
-              "background-image",
-              "url(" +
-                selectors.item
-                  .last()
-                  .find(selectors.img)
-                  .attr("src") +
-                ")"
-            );
-            selectors.item.last().addClass(selectors.activeClass);
-          } else if (pos <= max - 40  && pos >= min) {
-            selectors.id.css(
-              "background-image",
-              "url(" +
-                $(this)
-                  .find(selectors.img)
-                  .attr("src") +
-                ")"
-            );
-            selectors.item.removeClass(selectors.activeClass);
-            $(this).addClass(selectors.activeClass);
-          }
-        });
-      });
-    };
-  })(jQuery);
-  
-  $("#timeline-1").timeline();
+const startSwiper = () => {
+    const timelineSwiper = new Swiper(".swiper-container", {
+        direction: "vertical",
+        slidesPerView: 1,
+        speed: 1600,
+        height: 300,
+        effect: "flip",
+        pagination: '.swiper-pagination',
+        paginationBulletRender: function (swiper, index, className) {
+          let year = document.querySelectorAll('.swiper-slide')[index].querySelector(".timeline-year").innerText;
+          return '<span class="' + className + '">' + year + '</span>';
+        },
+        paginationClickable: true,
+        nextButton: ".swiper-button-next",
+        prevButton: ".swiper-button-prev"
+    });
+}
--- scr/index.js
+++ scr/index.js
@@ -2,7 +2,15 @@
 document.querySelector("#createContact").addEventListener("click",() => showContact());
 document.querySelector(".menu").addEventListener("click",function(){this.classList.toggle('open')})
 
-window.onload = async () => {
+window.onload = () => {
+    welcomeMesagge();
+    insertItemsTimeline();
+    startSwiper();
+}
+
+const wait = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+const welcomeMesagge = async () => {
     await writeLine("Hola!","#9CD9F0");
     await writeLine("Soy Alberto Soto","#CDEE69");
     await writeLine("Desarrollador de Software","#CDEE69");
@@ -11,4 +19,33 @@ window.onload = async () => {
     await writeLine(">","#fff",false,false);
 }
 
-const wait = ms => new Promise(resolve => setTimeout(resolve, ms));
+const insertItemsTimeline = async () => {
+    let item2021 = {
+        img:"url(https://unsplash.it/1920/500?image=11)",
+        id: 2021,
+        year: 2021,
+        title: "Avanzando por el Mundo",
+        text: "Me encuentro en constante capacitacion, buscando mejorar mis capacidades para poder ofrecer mas y mejores soluciones"
+    }
+
+    let item2019 = {
+        img:"url(https://unsplash.it/1920/500?image=12)",
+        year: 2019,
+        title: "Inicio mis Estudios Universitarios",
+        text: "Aun que siempre e preferido la autonomia, hay momentos en los que se requiere una guia, y la Universidad Abierta y a Distancia de México, me ofrece una mezacla perfecta de ambas cosas."
+    }
+
+    let item2016 = {
+        img:"url(https://unsplash.it/1920/500?image=13)",
+        year: 2016,
+        title: "Mi primer proyecto Web",
+        text: "Tuve la oportunidad de realizar un proyecto web para poder agilizar mis actividades, este me permitio hacer mi curiosidad mas grande y poder conocer tecnologiass como PHP y MySQL."
+    }
+
+    let arData = [item2021,item2019,item2016];
+    for(let element of arData){
+        elementTimeline(element);
+    }
+    await wait(1000);
+}
+
